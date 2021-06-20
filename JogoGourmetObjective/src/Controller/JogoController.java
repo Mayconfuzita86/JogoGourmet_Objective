9599a8b8f117118164d6b41f0fdbdb0b6a1aaf36
mayconfuzita <mayconfuzita@gmail.com>
2021-06-20 14:43:05 -0300

    tests ok

--- JogoGourmetObjective/src/Controller/JogoController.java
+++ JogoGourmetObjective/src/Controller/JogoController.java
@@ -23,50 +23,97 @@ public class JogoController {
     public void iniciarJogo() {
         percorrerNo(raiz);
     }
-
-    public void validaNoSim(NoModel noSim) {
-        if (noSim == null) {
-            venceuJogo();
-        } else {
-            percorrerNo(noSim);
-        }
-    }
-
-    public void validaNoNao(NoModel noNao) {
-        if (noNao == null) {
-            perdeuJogo();
-        } else {
-            percorrerNo(noNao);
-        }
-    }
-
-    public void percorrerNo(NoModel no) {
+    
+    public void percorrerNo(NoModel no){
         noAtual = no;
-        
-        if (perguntarPrato(no.getDescricao())) {
-            validaNoSim(no.getFilhoSim());
-        } else {
-            validaNoNao(no.getFilhoNao());
+        if(perguntarPrato(no.getDescricao())){
+            validaNoFilhoSim(no.getFilhoSim());
+        }else{
+            validaNoFilhoNao(no.getFilhoNao());
         }
+    }
+
+    public void validaNoFilhoSim(NoModel noSim){
+        if(noSim == null){
+            venceuJogo();
+            return;
+        }
+        percorrerNo(noSim);
+    }
+
+    public void validaNoFilhoNao(NoModel noNao) {
+        if(noNao == null){
+            perdeuJogo();
+            return;
+        }
+        percorrerNo(noNao);
+    }
+
+    public NoModel criaNovoRamo(String descricaoPrato, String caracteristicaPrato) {
+
+       NoModel noCaracteristica = new NoModel(caracteristicaPrato);
+       NoModel noPrato = new NoModel(descricaoPrato);
+       noCaracteristica.setFilhoSim(noPrato);
+       return noCaracteristica;
+    }
+
+    public void incluirRamificacao(NoModel novoRamo){
+
+        noAtual.setFilhoNao(novoRamo);
+        novoRamo.setPai(noAtual);
+    }
+
+    public void incluirRamificacaoRealocandoFolha(NoModel novoRamo){
+
+        NoModel noAvo = noAtual.getPai();
+        NoModel noPai = novoRamo;
+        NoModel noFilho = noAtual;
+
+        if(verificaSeEhLasanha() && verificaSePaiEhRaiz())
+            noAvo.setFilhoSim(noPai);
+        else
+            noAvo.setFilhoNao(noPai);
+
+        noPai.setPai(noAvo);
+
+        noPai.setFilhoNao(noFilho);
+        noFilho.setPai(noPai);
     }
 
     public void perdeuJogo() {
+
         String novoPrato = perguntaPratoQuePensou();
         String caracteristicaPrato = caracteristicaDoNovoPrato(novoPrato, noAtual.getDescricao());
 
-        NoModel novoNoSuperior = new NoModel(caracteristicaPrato);
-        novoNoSuperior.setPai(noAtual.getPai());
-        
-        NoModel novoNoSim = new NoModel(novoPrato);
-        novoNoSuperior.setFilhoSim(novoNoSim);
-        
-        if (noAtual.getPai().getFilhoSim().equals(noAtual)) {
-            novoNoSuperior.getPai().setFilhoSim(novoNoSuperior);
-        } else {
-            novoNoSuperior.getPai().setFilhoNao(novoNoSuperior);
-        }
+        NoModel novoRamo = criaNovoRamo(novoPrato, caracteristicaPrato);
 
-        noAtual.setPai(novoNoSuperior);
-        novoNoSuperior.setFilhoNao(noAtual);
+       if(verificaSeDeveRealocarFolha()){
+           incluirRamificacaoRealocandoFolha(novoRamo);
+           return;
+       }
+       incluirRamificacao(novoRamo);
     }
+
+    public boolean verificaSeEhBolo(){
+        if(noAtual.getDescricao().equals(NoModel.FOLHA_PADRAO_NAO)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verificaSeEhLasanha(){
+        if(noAtual.getDescricao().equals(NoModel.FOLHA_PADRAO_SIM)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verificaSeDeveRealocarFolha(){
+        return verificaSeEhLasanha() || verificaSeEhBolo();
+    }
+
+    public boolean verificaSePaiEhRaiz(){
+        return noAtual.getPai().getDescricao().equals(raiz.getDescricao());
+    }
+
 }
